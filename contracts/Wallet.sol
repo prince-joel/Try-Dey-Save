b9fb6f551cc9666d4f3e5acb0e921b0d31d3eaf8
Your Name <you@example.com>
2022-07-27 22:33:25 -1200

    some changes

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,75 +1,74 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
-import "./project.sol";
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract IWallet is PriceConsumerV3 {
-
-    // ERC20 public token;
-    uint public stakeTime;
-    // uint public initialDeposit;
-    uint public investAmount;
-    address public owner;
-    // uint public currentBalance;
-
+contract IWallet  {
     event Deposit(address caller, uint amount);
-    event Stake(address caller, uint amount);
+    event Invest( uint amount);
     event Withdraw(address caller, uint amount);
-     event WithdrawInvestment(address caller, uint amount);
+    event WithdrawInvestment(address caller, uint amount);
+    uint public investTime;
+    uint public investAmount;
 
-    mapping(address => uint) accountBalance;
-    mapping(address => uint) ibalance;
-    mapping(address => uint) currentIBalance;
+    struct Users{
+        address owner;
+        uint  accountBalance;
+        uint  currentIBalance;
+        bool hasEarned;
+    }
 
-    constructor() {
-       owner = payable (msg.sender);
-   }
-
+    mapping(address => Users) public user;
+    
 
     function deposit()payable  external {
        require(msg.value >= 1 ether, "invalid amount");
        investAmount = msg.value * 10 / 100 ;
        uint initialDeposit = msg.value - investAmount;
-       accountBalance[msg.sender] = accountBalance[msg.sender] + initialDeposit;
+       Users storage user1 = user[msg.sender];
+       user1.owner = msg.sender; 
+       user1.accountBalance += initialDeposit; 
+       user1.currentIBalance += investAmount;       
        emit Deposit(msg.sender, msg.value);
-
    }
 
    function withdraw(uint _amount) external {
-       require(msg.sender == owner, "caller is not owner");
-       require(_amount <= accountBalance[msg.sender], "invalid amount");
-       accountBalance[msg.sender] = accountBalance[msg.sender] - _amount;
+       Users storage user1 = user[msg.sender];
+       user1.owner = msg.sender;
+       require(user1.owner == msg.sender, "caller is not owner");
+       require(_amount <= user1.accountBalance, "invalid amount");
+       user1.accountBalance = user1.accountBalance - _amount;
        payable(msg.sender).transfer(_amount);
        emit Withdraw(msg.sender, _amount);
 
    }
 
-    function stake() payable external{
+    function invest() payable external{
         require(msg.value == investAmount, "invalid amount");
-        stakeTime = block.timestamp;
-        ibalance[msg.sender] = ibalance[msg.sender] + msg.value;
-        emit Stake(msg.sender, msg.value);
+        investTime = block.timestamp;
+        Users storage user1 = user[msg.sender];
+        user1.currentIBalance = user1.currentIBalance + msg.value;
+        user1.hasEarned = false;
+        emit Invest( msg.value);
 
     }
 
     function earn() external returns(uint){
-        require(block.timestamp - stakeTime >= 20 seconds , "cant earn now");
-        uint interest = ibalance[msg.sender] * 20/100;
-         currentIBalance[msg.sender] = ibalance[msg.sender] + interest;
-         return currentIBalance[msg.sender];
+        Users storage user1 = user[msg.sender];
+        require(user1.hasEarned == false, "cant earn again");
+        require(block.timestamp - investTime >= 20 seconds , "cant earn now");
+        uint interest = investAmount * 20/100;
+        user1.currentIBalance = user1.currentIBalance + interest;
+        user1.hasEarned = true;
+        return user1.currentIBalance;
     }
 
+//         // require(block.timestamp - investTime >= 20 seconds , "cant withdraw now");
     function withdrawInvestment(uint amount) external {
-        require(block.timestamp - stakeTime >= 20 seconds , "cant withdraw now");
-        currentIBalance[msg.sender] = currentIBalance[msg.sender] - amount;
+        Users storage user1 = user[msg.sender];
+        require(amount <= user1.currentIBalance, "invalid amount");
+        user1.currentIBalance = user1.currentIBalance - amount;
+        user1.accountBalance = user1.accountBalance + amount;    
         payable(msg.sender).transfer(amount);
         emit WithdrawInvestment(msg.sender, amount);
-
     }
 
-    
-
-    function getBalance() external view returns (uint, uint) {
-       return (address(this).balance, msg.sender.balance);
-   }
 }
